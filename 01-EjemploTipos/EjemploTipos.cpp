f6bf15e9a34e3f3164849ba8ec1d17caca822dea
Fedoedo <82189862+Fedoedo@users.noreply.github.com>
2021-05-06 18:41:35 -0300

    Add files via upload

---
+++ 01-EjemploTipos/EjemploTipos.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cassert>
+using std::string ;
+
+  int main () {
+
+  bool Booleana=true;
+  char Caracter='y';
+  unsigned Unsigned=90;
+  int Entero=-4;
+  double Doble=9.45;
+  string Cadena="test";
+
+ assert (Booleana==true);
+ assert (Caracter!='x');
+ assert (Unsigned!=40);
+ assert (Entero==-4);
+ assert (Doble==9.45);
+ assert (Cadena=="test");
+
+ }
